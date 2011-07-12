c600151b42cbbee9a5816d4e11c7433fba0f1705
Chiku <chirantan.mitra@gmail.com>
2011-07-13 00:46:29 +0530

    New run.

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+rm */output.txt
+rm */*.csv
+rm output/*.markdown
+
 echo "****** RUBY ******"
 cd ruby
-ruby estimate_pi.rb
+./estimate_pi.rb | tee output.txt
 cd ..
 echo "****** RUBY ******"
 
@@ -12,7 +16,7 @@ echo ""
 echo "****** C++ ******"
 cd c++
 g++ estimate_pi.cpp -o estimate_pi
-./estimate_pi
+./estimate_pi | tee output.txt
 cd ..
 echo "****** C++ ******"
 
