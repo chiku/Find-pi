196d26104bc6368d4e4d79a221e1dd7654249b51
Chiku <chirantan.mitra@gmail.com>
2011-07-13 00:17:33 +0530

    Do not over-write old language data in markdown with the latest ones

--- create_markdown.rb
+++ create_markdown.rb
@@ -8,20 +8,23 @@ SAMPLE_SIZE = [1, 10, 100, 1_000, 10_000, 100_000].map(&:to_s)
 Dir.glob('**/*.csv').each do |file|
   language, category, extension = file.split "."
   language = File.basename(language).capitalize
-  collection[category] = [language]
+  collection[category] ||= {}
+  collection[category][language] ||= []
 
   data = CSV::read file
   SAMPLE_SIZE.each do |traced_size|
     value = data.detect {|d| d.first == traced_size}
-    collection[category] << (value ? "#{value.last}%" : "N.A.")
+    collection[category][language] << (value ? "#{value.last}%" : "N.A.")
   end
 end
 
-collection.each do |category, data|
+collection.each do |category, values|
   f = File.open "output/#{category}.markdown", "w+"
   f.write "| Language | " + SAMPLE_SIZE.join(" | ") + " |\n"
   f.write "| --- | " + " ---:|" * SAMPLE_SIZE.size + "\n"
-  f.write "| " + data.join(" | ") + " |\n"
+  values.each do |language, data|
+    f.write "| " + language + " | " + data.join(" | ") + " |\n"
+  end
   f.close
 end
 
