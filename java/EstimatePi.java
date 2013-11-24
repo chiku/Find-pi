fa1af26dcc741d6d4e9f0803adeff83026888fdf
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-11-24 22:25:13 +0530

    Idiomatic java

--- java/EstimatePi.java
+++ java/EstimatePi.java
@@ -2,7 +2,7 @@ class EstimatePi
 {
     static int SAMPLE_SIZES[] = {1, 10, 100, 1000, 10000, 100000};
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
     for(int sampleSize : SAMPLE_SIZES)
         new ApproxPiReporter(sampleSize).output();
     }
