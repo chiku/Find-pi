db9818e4afb2506a265b8a8282d25b8d11c52d0a
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-12-04 16:16:06 +0530

    Implement ApproxPiReporter as revealing-prototype
    Third commit to move javascript implementation from revealing-module pattern to revealing-prototype pattern
    
    This didn't change the execution time by much (less that 0.2 seconds, 37.8 to 37.7s)

--- javascript/approx.pi.reporter.js
+++ javascript/approx.pi.reporter.js
@@ -4,74 +4,86 @@ var approxPiFinder = require('./approx.pi.finder'),
     util           = require('util');
 
 var ApproxPiReporter = function(sampleSize) {
-  var sampleSize   = sampleSize,
-      REPEATS      = 1000,
-      lowestError  = undefined,
-      highestError = undefined,
-      sumOfPis     = undefined,
-      bestPi       = undefined,
-      worstPi      = undefined,
-      averagePi    = undefined,
-      averageError = undefined;
-
-  var generate = function() {
-    var finder = approxPiFinder.ApproxPiFinder(sampleSize);
-    var approxPi = finder.approxPi,
-        error    = finder.error;
-
-    if (Math.abs(error) <= Math.abs(lowestError)) {
-      bestPi = approxPi;
-      lowestError = error;
+    if (!(this instanceof ApproxPiReporter)) {
+        return new ApproxPiReporter(sampleSize);
     }
-    if (Math.abs(error) >= Math.abs(highestError)) {
-      worstPi = approxPi;
-      highestError = error;
-    }
-    sumOfPis = sumOfPis + approxPi;
-  };
 
-  var repeatGenerates = function() {
-    lowestError = 100.0;
-    highestError = 0.0;
-    sumOfPis = 0.0;
-    for (var i = 0; i < REPEATS; i++) {
-      generate();
-    }
-    averagePi = sumOfPis / REPEATS;
-    averageError = (averagePi - Math.PI) * 100.0 / Math.PI;
-  };
+    this.sampleSize   = sampleSize;
 
-  var output = function() {
-    repeatGenerates();
-    outputToConsole();
-    outputToFiles();
-  };
+    this.lowestError  = undefined;
+    this.highestError = undefined;
+    this.averageError = undefined;
 
-  var outputToConsole = function() {
-    console.log("\n")
-    console.log("For sample size of " + sampleSize);
-    console.log("Best PI  : " + bestPi + " (error = " + lowestError + "%)");
-    console.log("Worst PI : " + worstPi + " (error = " + highestError + "%)");
-    console.log("Avg. PI  : " + averagePi + " (error = " + averageError +"%)");
-  };
-
-  var outputToFiles = function() {
-    writeToFile("../output/javascript.average.csv", averageError);
-    writeToFile("../output/javascript.best.csv", lowestError);
-    writeToFile("../output/javascript.worst.csv", highestError);
-  };
-
-  var writeToFile = function(fileName, property) {
-    fs.open(fileName, "a+", function(err, fd) {
-      fs.write(fd, sprintf.sprintf("%d,%.4f\n", sampleSize, property));
-      fs.close(fd);
-    });
-  };
-
-  return {
-    output: output
-  };
+    this.sumOfPis     = undefined;
+    this.bestPi       = undefined;
+    this.worstPi      = undefined;
+    this.averagePi    = undefined;
 };
+
+ApproxPiReporter.prototype = (function() {
+  var REPEATS = 1000,
+
+      generate = function() {
+          var finder = approxPiFinder.ApproxPiFinder(this.sampleSize),
+              approxPi = finder.approxPi,
+              error = finder.error;
+
+            if (Math.abs(error) <= Math.abs(this.lowestError)) {
+                this.bestPi = approxPi;
+                this.lowestError = error;
+            }
+            if (Math.abs(error) >= Math.abs(this.highestError)) {
+                this.worstPi = approxPi;
+                this.highestError = error;
+            }
+            this.sumOfPis += approxPi;
+        },
+
+        repeatGenerates = function() {
+            var i;
+            this.lowestError = 100.0;
+            this.highestError = 0.0;
+            this.sumOfPis = 0.0;
+
+            for (i = 0; i < REPEATS; i+=1) {
+                generate.call(this);
+            }
+
+            this.averagePi = this.sumOfPis / REPEATS;
+            this.averageError = (this.averagePi - Math.PI) * 100.0 / Math.PI;
+        },
+
+        output = function() {
+            repeatGenerates.call(this);
+            outputToConsole.call(this);
+            outputToFiles.call(this);
+        },
+
+        outputToConsole = function() {
+            console.log("\n");
+            console.log("For sample size of " + this.sampleSize);
+            console.log("Best PI  : " + this.bestPi + " (error = " + this.lowestError + "%)");
+            console.log("Worst PI : " + this.worstPi + " (error = " + this.highestError + "%)");
+            console.log("Avg. PI  : " + this.averagePi + " (error = " + this.averageError +"%)");
+        },
+
+        outputToFiles = function() {
+            writeToFile.call(this, "../output/javascript.average.csv", this.averageError);
+            writeToFile.call(this, "../output/javascript.best.csv", this.lowestError);
+            writeToFile.call(this, "../output/javascript.worst.csv", this.highestError);
+        },
+
+        writeToFile = function(fileName, property) {
+            var sampleSize = this.sampleSize;
+            fs.open(fileName, "a+", function(err, fd) {
+                fs.write(fd, sprintf.sprintf("%d,%.4f\n", sampleSize, property));
+            });
+        };
+
+    return {
+        output: output
+    };
+}());
 
 exports.ApproxPiReporter = ApproxPiReporter;
 
