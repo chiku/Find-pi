3c9721c7bef8d9fc1d3627cc2208a2bee87b6fbf
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-12-04 19:53:54 +0530

    Detect alternative versions correctly

--- lua/run.sh
+++ lua/run.sh
@@ -4,8 +4,13 @@
 
 LUA=`which luajit2 2> /dev/null`
 if [ -z "$LUA" ] ; then
+  LUA=`which luajit 2> /dev/null`
+fi
+if [ -z "$LUA" ] ; then
   LUA=`which lua 2> /dev/null`
-elif [ -n "$LUA" ] ; then
+fi
+
+if [ -n "$LUA" ] ; then
   display "Lua"
   echo "Using $LUA"
   $LUA estimate_pi.lua | tee -a ../output/lua.txt
--- python/run.sh
+++ python/run.sh
@@ -5,11 +5,15 @@
 PYTHON=`which pypy 2> /dev/null`
 if [ -z "$PYTHON" ] ; then
   PYTHON=`which python3 2> /dev/null`
-elif [ -z "$PYTHON" ] ; then
+fi
+if [ -z "$PYTHON" ] ; then
   PYTHON=`which python 2> /dev/null`
-elif [ -z "$PYTHON" ] ; then
+fi
+if [ -z "$PYTHON" ] ; then
   PYTHON=`which python2 2> /dev/null`
-elif [ -n "$PYTHON" ] ; then
+fi
+
+if [ -n "$PYTHON" ] ; then
   display "Python"
   echo "Using $PYTHON"
   $PYTHON estimate_pi.py | tee -a ../output/python.txt
